ae04532c9f16ef10c186fe94f78232692e251a07
qqq <qqq@mail.com>
2021-10-14 17:21:05 +0200

    add reflection class

---
+++ src/com/urise/webapp/reflection/MainReflection.java
@@ -0,0 +1,12 @@
+package com.urise.webapp.reflection;
+
+import com.urise.webapp.model.Resume;
+
+public class MainReflection {
+
+    private static Resume resume = new Resume();
+
+    public static void main(String[] args) {
+
+    }
+}

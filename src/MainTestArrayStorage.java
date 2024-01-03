48b743e5d8026eddbfc2837c8ee42a712025463f
laborant <stupidl@mail.com>
2024-01-03 15:53:16 +0100

    HW1 done

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -28,6 +28,7 @@ public class MainTestArrayStorage {
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
+        extendedTest();
     }
 
     static void printAll() {
@@ -35,5 +36,41 @@ public class MainTestArrayStorage {
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
         }
+    }
+
+    static void extendedTest() {
+        System.out.println("Extended test");
+        System.out.println("Test 1. Delete all resumes with given uuid");
+        ARRAY_STORAGE.clear();
+        Resume r1 = new Resume();
+        r1.uuid = "first";
+        Resume r2 = new Resume();
+        r2.uuid = "second";
+        Resume r3 = new Resume();
+        r3.uuid = "second";
+        Resume r4 = new Resume();
+        r4.uuid = "third";
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r4);
+        printAll();
+        System.out.println("Delete elements with uuid = second");
+        ARRAY_STORAGE.delete("second");
+        printAll();
+
+        System.out.println("Test2. Delete non existing uuid");
+        ARRAY_STORAGE.clear();
+        Resume r1341314 = new Resume();
+        r1341314.uuid = "i dont care";
+        ARRAY_STORAGE.save(r1341314);
+        ARRAY_STORAGE.delete("blabla");
+        printAll();
+
+        System.out.println("Test 2. Delete from empty storage");
+        ARRAY_STORAGE.clear();
+        ARRAY_STORAGE.delete("12345");
+        printAll();
+
     }
 }

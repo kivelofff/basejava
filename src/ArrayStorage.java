c98c4a78888167ecf8724dba1371e205a2f252fb
laborant <stupidl@mail.com>
2024-01-05 00:43:17 +0100

    fix HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,7 +8,7 @@ public class ArrayStorage {
     private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
